6f391bd3d6fbe7127a6d32ec415ab48c7aa5b0e4
yasminlucero <yasmin.lucero@gmail.com>
2012-12-08 12:04:02 -0800

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -1,5 +1,8 @@
 ## Card Shuffler
-##
+## accepts a list of integers 1-52, representing a deck of cards
+## returns a well shuffledDeck (also a list of integers)
+## Dec 6 2012
+## contact: yasmin.lucero@gmail.com
 
 def shuffleDeck(startDeck):
 # input should be a list with integers 1-52 arranged in any order without repeats
